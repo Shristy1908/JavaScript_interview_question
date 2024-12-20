83b60426d320667b9e5aaa3de57b6440d8f0746b
Shristy Kumari <82372346+Shristy1908@users.noreply.github.com>
2024-12-20 23:15:34 +0530

    Update ConcatSubArrayElement.js

--- ConcatSubArrayElement.js
+++ ConcatSubArrayElement.js
@@ -1,16 +1,16 @@
 var arr = [12, 21, [1, 3], [2], [34, [13]]];
 
-var resultarr = [];
+var resultarr1 = [];
 for (var i = 0; i < arr.length; i++) {
   if (arr[i].length >= 1) {
     for (var j = 0; j < arr[i].length; j++) {
-      resultarr.push(arr[i][j]);
+      resultarr1.push(arr[i][j]);
     }
   } else {
-    resultarr.push(arr[i]);
+    resultarr1.push(arr[i]);
   }
 }
 
-resultarr.map((ele)=>{
+resultarr1.map((ele)=>{
     console.log(ele);
 });
