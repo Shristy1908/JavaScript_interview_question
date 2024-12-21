782538933dea17fc5ec7f948e57c9c485cb2e3fa
Shristy Kumari <82372346+Shristy1908@users.noreply.github.com>
2024-12-22 00:38:11 +0530

    Add files via upload

---
+++ MedianOfTwoSortedArray.js
@@ -0,0 +1,16 @@
+let nums1 = [1, 3, 2, 4, 5];
+let nums2 = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+   let arr1 = [...nums1, ...nums2];
+   console.log(arr1);
+
+   arr1.sort();
+   console.log(arr1);
+   
+   if (arr1.length % 2 == 1) {
+     console.log(arr1[Math.floor(arr1.length / 2)]);
+   } else {
+     console.log(
+       (arr1[(arr1.length / 2) -1] + arr1[arr1.length / 2 ]) / 2
+     );
+   }
