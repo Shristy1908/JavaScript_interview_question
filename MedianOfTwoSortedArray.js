f266d3f5369ddbd224a8dcc1c7025e9eaf6e8d7f
Shristy Kumari <82372346+Shristy1908@users.noreply.github.com>
2024-12-22 00:40:06 +0530

    Update MedianOfTwoSortedArray.js

--- MedianOfTwoSortedArray.js
+++ MedianOfTwoSortedArray.js
@@ -4,7 +4,7 @@ let nums2 = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
    let arr1 = [...nums1, ...nums2];
    console.log(arr1);
 
-   arr1.sort();
+   arr1.sort((a,b)=>(a-b));
    console.log(arr1);
    
    if (arr1.length % 2 == 1) {
