d8678c2f0e67d7958d38dd588cb7b496662b5d4d
Shristy Kumari <82372346+Shristy1908@users.noreply.github.com>
2024-12-23 00:29:28 +0530

    Add files via upload

---
+++ Anagram.js
@@ -0,0 +1,16 @@
+var s = "shristy";
+var t = "ystirst";
+
+if (s.length != t.length) {
+  console.log("Not anagram");
+} 
+else {
+  var s1 = s.split("").sort().join("");
+  var t1 = t.split("").sort().join("");
+
+  if (s1 === t1) {
+    console.log("Anagram");
+  } else {
+    console.log("Not anagram");
+  }
+}
