a6e1381eaf07305ff00a30c7e35eae96ec8b0115
Shristy Kumari <82372346+Shristy1908@users.noreply.github.com>
2025-01-01 00:04:47 +0530

    Add files via upload

---
+++ CapitalizeTheFirstLtrOfStr.js
@@ -0,0 +1,9 @@
+let str="hi i am shristy roy";
+
+let strArr=str.split(" ") ;
+
+const newarr=strArr.map((ele)=>{
+    return ele[0].toUpperCase()+ele.slice(1);
+})
+const resStr=newarr.join(' ')
+console.log(resStr);
