96a007b51aab06f13787cfb9d0a4fa3c343874c7
Shristy Kumari <82372346+Shristy1908@users.noreply.github.com>
2025-02-03 20:09:59 +0530

    Update ReverseString.js

--- ReverseString.js
+++ ReverseString.js
@@ -1,3 +1,5 @@
+//without using build-in methods
+
 let str="Shristy";
 
 let revseStr="";
@@ -6,3 +8,7 @@ for(var i=str.length-1;i>=0;i--){
 }
 
 console.log(revseStr);
+
+// using built-in methods
+
+let splitstr=str.split("").reverse().join("");
