e23ad95cf618dada180fa761e6d85be6647f29af
Shristy Kumari <82372346+Shristy1908@users.noreply.github.com>
2025-02-04 22:16:01 +0530

    Update StringPalinedrome.js

--- StringPalinedrome.js
+++ StringPalinedrome.js
@@ -10,3 +10,35 @@ if(str==str2){
 else{
     console.log("Not Palinedrome No");
 }
+
+
+
+// another approach
+
+
+let str = "Noon";
+
+str=str.toLowerCase();
+
+let leftside=0;
+let rightside=str.length-1;
+let flag=1;
+
+function checkPalindrome(){
+    while(rightside>leftside){    // 3>0  2>1 1>2
+    if(str[leftside]!==str[rightside]){   
+        return flag=0;
+    }
+    leftside++;
+    rightside--;
+}
+ return flag;
+}
+
+checkPalindrome(str);
+if(flag==1){
+   console.log("palindrome no")
+}
+else{
+    console.log("not a palindrome no")
+}
